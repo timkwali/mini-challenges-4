c273f4169d9c72e8cd95cde03829e754239d13b6
Timring <timringperez@gmail.com>
2020-05-23 11:45:34 +0100

    initial commit

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,9 @@
-function countIslands(grid) {}
+function countIslands(grid) {
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[i].length; j++) {
+
+    }
+  }
+}
 
 module.exports = countIslands;
